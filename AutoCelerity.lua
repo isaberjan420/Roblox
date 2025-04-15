8acf0b9a14a8f7dd729b72aa36fadbd196165993
isaberjan420 <isaberjan420@gmail.com>
2025-04-15 19:51:06 +0300

    Update AutoCelerity.lua

--- AutoCelerity.lua
+++ AutoCelerity.lua
@@ -1,5 +1,5 @@
-if game.PlaceId == 6403373529 then
+if game.PlaceId == 6403373529 then --normal sb server
     loadstring(game:HttpGet("https://raw.githubusercontent.com/isaberjan420/Roblox/refs/heads/main/Slap%20Battles/CelerityOne.lua"))()
-elseif game.PlaceId == 124596094333302 then
+elseif game.PlaceId == 124596094333302 then --new player sb server
     loadstring(game:HttpGet("https://raw.githubusercontent.com/isaberjan420/Roblox/refs/heads/main/Slap%20Battles/CelerityOne.lua"))()
 end
